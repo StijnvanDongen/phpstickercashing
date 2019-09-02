d5ad22baba541271fa6d7f03c315e34786587158
Robin <robinbrongers2001@gmail.com>
2019-09-02 10:18:25 +0200

    Nu klopt de login ook

--- login.php
+++ login.php
@@ -15,23 +15,25 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
     <title>Inloggen</title>
 </head>
 <body>
-<form action=""><p>Inloggen</p>
+    <div class="login">
+        <form action=""><p>Inloggen</p>
+            <div class="form-group">
+                <p>Email</p>
+                <input type="email" name="email" placeholder="Voer je email in">
+            </div>
+            <div class="form-group">
+                <p>Wachtwoord</p>
+                <input type="password" name="psw" placeholder="Voer hier je wachtwoord in">
+            </div>
 
-    <div class="form-group">
-        <p>Email</p>
-        <input type="email" name="email" placeholder="Voer je email in">
+            <input class="login" type="submit" value="Inloggen">
+
+        </form>
     </div>
-    <div class="form-group">
-        <p>Wachtwoord</p>
-        <input type="password" name="psw" placeholder="Voer hier je wachtwoord in">
-    </div>
-
-    <input class="login" type="submit" value="Inloggen">
-
-</form>
 </body>
 </html>
