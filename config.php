f5992e5791a7339c5cefa2923e7c68343286befa
Stijn van Dongen <Stijn_vandongen@outlook.com>
2019-09-02 10:04:50 +0200

    config.php added

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php session_start();
+
+$dbHost = 'localhost';
+$dbName = 'sticker_cashing';
+$dbUser = 'root';
+$dbPass = '';
+
+try {
+    $db = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    echo " <h1>OEI</h1> de connectie met de database is niet gelukt check je config.php  ";
+    die($e->getMessage());
+}
