03be8e8a1595f81c9629093aeac8b53b96d75b88
Unknown <charlotte.oers@kpnmail.nl>
2019-09-02 09:49:14 +0200

    Login pagina aangemaakt

---
+++ login.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Charlotte van Oers
+ * Date: 2-9-2019
+ * Time: 09:41
+ */
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Inloggen</title>
+</head>
+<body>
+<form action=""><p>Inloggen</p>
+
+    <div class="form-group">
+        <p>Email</p>
+        <input type="email" name="email" placeholder="Voer je email in">
+    </div>
+    <div class="form-group">
+        <p>Wachtwoord</p>
+        <input type="password" name="psw" placeholder="Voer hier je wachtwoord in">
+    </div>
+
+    <input class="login" type="submit" value="Inloggen">
+
+</form>
+</body>
+</html>
