1e249fd4641e65982ceb14ff4d8b41751bd9605a
Unknown <charlotte.oers@kpnmail.nl>
2019-09-09 12:41:20 +0200

    api locatie gebruiker gefixt

--- api.php
+++ api.php
@@ -9,74 +9,107 @@
     require 'footer.php';
 
 
+
 ?>
 
 
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="./css/main.css">
-
-    <title>Google maps </title>
+    <title>Geolocation</title>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
+    <style>
+        /* Always set the map height explicitly to define the size of the div
+         * element that contains the map. */
+        #map {
+            height: 100%;
+        }
+        /* Optional: Makes the sample page fill the window. */
+        html, body {
+            height: 100%;
+            margin: 0;
+            padding: 0;
+        }
+    </style>
 </head>
 <body>
-    <h1>Locaties met QR codes</h1>
-    <style>
-    #map {
-    height: 100%;
-    }
-    /* Optional: Makes the sample page fill the window. */
-    html, body {
-    height: 100%;
-    margin: 0;
-    padding: 0;
-    }
-
-</style>
-    <div id="map"></div>
-    <script>
-        // Note: This example requires that you consent to location sharing when
-        // prompted by your browser. If you see the error "The Geolocation service
-        // failed.", it means you probably did not give permission for the browser to
-        // locate you.
-        var map, infoWindow;
-        function initMap() {
-            map = new google.maps.Map(document.getElementById('map'), {
-                center: {lat: -34.397, lng: 150.644},
-                zoom: 6
-            });
-            infoWindow = new google.maps.InfoWindow;
-
-            // Try HTML5 geolocation.
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(function(position) {
-                    var pos = {
-                        lat: position.coords.latitude,
-                        lng: position.coords.longitude
-                    };
-
-                    infoWindow.setPosition(pos);
-                    infoWindow.setContent('Location found.');
-                    infoWindow.open(map);
-                    map.setCenter(pos);
-                }, function() {
-                    handleLocationError(true, infoWindow, map.getCenter());
-                });
-            } else {
-                // Browser doesn't support Geolocation
-                handleLocationError(false, infoWindow, map.getCenter());
-            }
+<div id="googleMap" style="width:100%;height:100%"></div>
+<button onclick="getlocation();"> Show Position</button>
+<script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDPr1C6cgCzPTg9AQiId7ZQiA9gXUo-kEQ&callback=googleMap">
+</script>
+<script>
+    function getlocation(){
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(showPos, showErr);
         }
+        else{
+            alert("Sorry! your Browser does not support Geolocation API")
+        }
+    }
+    //Showing Current Poistion on Google Map
+    function showPos(position){
+        lat = position.coords.latitude;
+        long = position.coords.longitude;
+        var lattlong = new google.maps.LatLng(lat, long);
+        var myOptions = {
+            center: lattlong,
+            zoom: 15,
+            mapTypeControl: true,
+            navigationControlOptions: {style:google.maps.NavigationControlStyle.SMALL}
+        };
+        var maps = new google.maps.Map(document.getElementById("googleMap"), myOptions);
+        var markers = new google.maps.Marker({position:lattlong, map:maps, title:"You are here!"});
+    }
+
+    //Handling Error and Rejection
+    function showErr(error) {
+        switch(error.code){
+            case error.PERMISSION_DENIED:
+                alert("User denied the request for Geolocation API.");
+                break;
+            case error.POSITION_UNAVAILABLE:
+                alert("USer location information is unavailable.");
+                break;
+            case error.TIMEOUT:
+                alert("The request to get user location timed out.");
+                break;
+            case error.UNKNOWN_ERROR:
+                alert("An unknown error occurred.");
+                break;
+        }
+    }
 
     </script>
+    <script>
+    function myMap() {
+        var myLatLng = {lat: 51.508742, lng: -0.120850};
 
-</script>
+        var mapProp= {
+            center: myLatLng,
+            zoom:5,
+        };
+        var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+        var marker = new google.maps.Marker({
+            position: (myLatLng)
+        });
+
+        marker.setMap(map);
+
+        // laat een infobalk zien boven de marker
+        var infowindow = new google.maps.InfoWindow({
+            content:"Engeland"
+        });
+
+        google.maps.event.addListener(marker, 'click', function () {
+            infowindow.open(map,marker);
+        });
+    }
+    </script>
     <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDPr1C6cgCzPTg9AQiId7ZQiA9gXUo-kEQ&callback=initMap">
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDPr1C6cgCzPTg9AQiId7ZQiA9gXUo-kEQ&callback=myMap">
     </script>
 </body>
 </html>
