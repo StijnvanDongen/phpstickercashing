5f8c75d96b0e778da8c713cf6334e4ce0229b5c9
Robin <robinbrongers2001@gmail.com>
2019-09-17 13:29:19 +0200

    Profiles styled

--- profiles.php
+++ profiles.php
@@ -5,30 +5,37 @@
  * Date: 12-9-2019
  * Time: 11:39
  */
-require 'config.php';
-$sql = "SELECT * FROM users";
-$query = $db->query($sql);
-$users = $query->fetchAll(PDO::FETCH_ASSOC);
 
-foreach ($users as $user){
-    $email = $user['email'];
-    $admin = $user['admin'];
-    $winkelier = $user['winkelier'];
+session_start();
+require 'header.php';
+if ($_SESSION['admin'] == 'admin') {
+    require 'config.php';
+    $sql = "SELECT * FROM users";
+    $query = $db->query($sql);
+    $users = $query->fetchAll(PDO::FETCH_ASSOC);
 
-    if ($admin == 0){
-        $admin = "is geen admin";
-    }else{
-        $admin = "is admin";
-    }
+    foreach ($users as $user) {
+        $email = $user['email'];
+        $admin = $user['admin'];
+        $winkelier = $user['winkelier'];
 
-    if ($winkelier == 0){
-        $winkelier = "is geen winkelier";
-    }else{
-        $winkelier = "is winkelier";
-    }
-    echo"
+        if ($admin == 0) {
+            $admin = "is geen admin";
+        } else {
+            $admin = "is admin";
+        }
+
+        if ($winkelier == 0) {
+            $winkelier = "is geen winkelier";
+        } else {
+            $winkelier = "is winkelier";
+        }
+        echo "
         <ul>
-            <li>$email $admin en $winkelier</li>   
+            <li style='font-weight: bold'>$email $admin en $winkelier</li>   
         </ul>
     ";
+    }
+}else{
+    header('Location: index.php');
 }
