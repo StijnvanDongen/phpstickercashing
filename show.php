8419249aa67335a55c13c5b0a097c31423649cb3
Robin <robinbrongers2001@gmail.com>
2019-09-19 11:27:54 +0200

    Ophalen van opject uit database

--- show.php
+++ show.php
@@ -5,3 +5,13 @@
  * Date: 19-9-2019
  * Time: 11:21
  */
+
+$id = $_GET['id'];
+
+$sql = "SELECT * FROM object WHERE id=:id";
+$prepare=$db->prepare($sql);
+$prepare->execute([
+    ':id' => $id
+]);
+
+$result = $prepare->fetch(PDO::FETCH_ASSOC);
