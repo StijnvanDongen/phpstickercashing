3a7491989865f7ed80cc8914362babf645922a24
Robin <robinbrongers2001@gmail.com>
2019-09-19 11:37:12 +0200

    wrwqf

---
+++ show_object.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Robin
+ * Date: 19-9-2019
+ * Time: 11:21
+ */
+
+session_start();
+
+if (isset($_SESSION['email'])) {
+
+    require 'config.php';
+
+    $id = $_GET['id'];
+
+    $sql = "SELECT * FROM objects WHERE id=:id";
+    $prepare = $db->prepare($sql);
+    $prepare->execute([
+        ':id' => $id
+    ]);
+
+    $result = $prepare->fetch(PDO::FETCH_ASSOC);
+    $picture = $result['picture'];
+
+    echo "<img src='$picture' alt='object' style='width: 20%; height: 40%; margin-bottom: 100px'>";
+
+    echo "<a href=''>Verzamel</a>";
+}else{
+    header('Location: index.php');
+}
+?>
+
+
