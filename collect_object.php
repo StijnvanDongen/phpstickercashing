72bddb0850e896ce2a0363e19ec7a204b67f6b27
Robin <robinbrongers2001@gmail.com>
2019-09-19 11:51:22 +0200

    Kan object maar 1 keer verzamelen

--- collect_object.php
+++ collect_object.php
@@ -11,12 +11,27 @@ require 'config.php';
 $email = $_GET['email'];
 $id = $_GET['id'];
 
-$sql = "INSERT INTO verzamelobjecten (email, object_id) VALUES (:email, :object_id)";
+$sql = "SELECT * FROM verzamelobjecten WHERE id=:id AND email=:email";
 $prepare = $db->prepare($sql);
 $prepare->execute([
-    ':email' => $email,
-    ':object_id' => $id
+    ':id' => $id,
+    ':email' => $email
 ]);
 
-header('Location: api.php');
+$result = $prepare->fetch(PDO::FETCH_ASSOC);
+
+if ($result){
+    echo"Je hebt dit al";
+}else {
+
+
+    $sql = "INSERT INTO verzamelobjecten (email, object_id) VALUES (:email, :object_id)";
+    $prepare = $db->prepare($sql);
+    $prepare->execute([
+        ':email' => $email,
+        ':object_id' => $id
+    ]);
+
+    header('Location: api.php');
+}
 
