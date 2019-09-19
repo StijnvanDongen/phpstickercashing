3e8c811d2fd47327132a375d3e0938511b5dcb60
Robin <robinbrongers2001@gmail.com>
2019-09-19 11:46:55 +0200

    Maybe it works

--- collect_object.php
+++ collect_object.php
@@ -6,6 +6,17 @@
  * Time: 11:37
  */
 
+require 'config.php';
+
 $email = $_GET['email'];
 $id = $_GET['id'];
+
+$sql = "INSERT INTO verzamelobjecten (email, object_id) VALUES (:email, :object_id)";
+$prepare = $db->prepare($sql);
+$prepare->execute([
+    ':email' => $email,
+    ':object_id' => $id
+]);
+
+header('Location: api.php');
 
