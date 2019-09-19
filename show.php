deeef3aa2270df01e273fb67ea960c41ed2799de
Robin <robinbrongers2001@gmail.com>
2019-09-19 11:32:04 +0200

    Image show from url

--- show.php
+++ show.php
@@ -5,13 +5,21 @@
  * Date: 19-9-2019
  * Time: 11:21
  */
+require 'config.php';
 
 $id = $_GET['id'];
 
-$sql = "SELECT * FROM object WHERE id=:id";
+$sql = "SELECT * FROM objects WHERE id=:id";
 $prepare=$db->prepare($sql);
 $prepare->execute([
     ':id' => $id
 ]);
 
 $result = $prepare->fetch(PDO::FETCH_ASSOC);
+$picture = $result['picture'];
+
+echo"<img src='$picture' alt='object'>"
+
+?>
+
+
